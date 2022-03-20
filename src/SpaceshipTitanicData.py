4e04b14b5b3a1bd232fb2c6d164e56037d154bf2
antonino-feitosa <antonino_feitosa@yahoo.com.br>
2022-03-20 19:26:03 -0300

    Fix NA in OneHotEncoding and Add log Transform

--- src/SpaceshipTitanicData.py
+++ src/SpaceshipTitanicData.py
@@ -4,8 +4,8 @@ Created on 13 de mar. de 2022
 @author: Antonino
 """
 
-from numpy import nan
-from pandas import DataFrame
+from numpy import nan, inf, log
+from pandas import DataFrame, read_csv, set_option
 from pandas.core.dtypes.missing import isna
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
@@ -17,12 +17,14 @@ class SpaceshipTitanicPreprocessing(BaseEstimator, TransformerMixin):
                  fill_missing=True,
                  fill_total_spend=True,
                  fill_percent_spend=True,
+                 apply_log=True,
                  scaling=True,
                  one_hot_encoding=True):
 
         self.fill_missing = fill_missing
         self.fill_percent_spend = fill_percent_spend
         self.fill_total_spend = fill_total_spend
+        self.apply_log = apply_log
         self.scaling = scaling
         self.one_hot_encoding = one_hot_encoding
         self.__cabin_deck_mode = None
@@ -33,7 +35,8 @@ class SpaceshipTitanicPreprocessing(BaseEstimator, TransformerMixin):
         self.__cryo_sleep_mode = None
         self.__cryo_grouped_median = None
         self.__age_grouped_median = None
-        self.__ohe_encoder = {'Destination': OneHotEncoder(), 'HomePlanet': OneHotEncoder()}
+        self.__ohe_encoder = {'Destination': OneHotEncoder(handle_unknown='ignore'),
+                              'HomePlanet': OneHotEncoder(handle_unknown='ignore')}
         self.__scaling_encoder = {
             'Age': MinMaxScaler(), 'CabinDeck': MinMaxScaler(), 'RoomService': MinMaxScaler(),
             'FoodCourt': MinMaxScaler(), 'ShoppingMall': MinMaxScaler(), 'Spa': MinMaxScaler(),
@@ -62,15 +65,25 @@ class SpaceshipTitanicPreprocessing(BaseEstimator, TransformerMixin):
 
         if self.one_hot_encoding:
             for name in self.__ohe_encoder.keys():
-                self.__ohe_encoder[name].fit(x[[name]])
+                feature = x[[name]].dropna() if self.fill_missing else x[[name]]
+                self.__ohe_encoder[name].fit(feature)
 
         if self.scaling:
             self.__scaling_encoder['CabinDeck'].fit(DataFrame([0, 7], columns=['CabinDeck']).dropna())
+            self.__scaling_encoder['Age'].fit(x[['Age']].dropna())
             if self.fill_total_spend:
                 total_spend = x['RoomService'] + x['FoodCourt'] + x['ShoppingMall'] + x['Spa'] + x['VRDeck']
-                self.__scaling_encoder['TotalSpend'].fit(DataFrame(total_spend, columns=['TotalSpend']).dropna())
-            for name in spend_features + ['Age']:
-                self.__scaling_encoder[name].fit(x[[name]].dropna())
+                total_spend = DataFrame(total_spend, columns=['TotalSpend']).dropna()
+                if self.apply_log:
+                    total_spend = log(total_spend)
+                    total_spend.replace(-inf, 0, inplace=True)
+                self.__scaling_encoder['TotalSpend'].fit(total_spend)
+            for name in spend_features:
+                feature = x[[name]].dropna()
+                if self.apply_log:
+                    feature = log(feature)
+                    feature.replace(-inf, 0, inplace=True)
+                self.__scaling_encoder[name].fit(feature)
 
         return self
 
@@ -97,8 +110,9 @@ class SpaceshipTitanicPreprocessing(BaseEstimator, TransformerMixin):
         spend_features = ['RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']
         if self.fill_missing:
             for name in spend_features:
-                x[name] = x[name].where(x['CryoSleep'], 0)
-                x[name].fillna(self.__cryo_grouped_median[name], inplace=True)
+                x[name] = x[['CryoSleep', name]].apply(
+                    lambda v: v[1] if not isna(v[1]) else (0 if v[0] else self.__cryo_grouped_median[name]),
+                    axis=1).to_numpy()
 
         if self.fill_total_spend:
             x['TotalSpend'] = x['RoomService'] + x['FoodCourt'] + x['ShoppingMall'] + x['Spa'] + x['VRDeck']
@@ -112,33 +126,37 @@ class SpaceshipTitanicPreprocessing(BaseEstimator, TransformerMixin):
             if self.fill_missing:
                 x[pct_spend_features] = x[pct_spend_features].fillna(0)  # clear pct/0
 
+        if self.apply_log:
+            x[spend_features] = log(x[spend_features])
+            x[spend_features] = x[spend_features].replace(-inf, 0)
+
         if self.fill_missing:
             x['Age'] = x[['HomePlanet', 'Age']].apply(lambda v: self.__age_grouped_median[v[0]] if isna(v[1]) else v[1],
                                                       axis=1).to_numpy()
 
         if self.one_hot_encoding:
             for name in self.__ohe_encoder.keys():
-                new_features = self.__ohe_encoder[name].get_feature_names_out()
+                new_features = self.__ohe_encoder[name].get_feature_names_out([name])
                 x[new_features] = self.__ohe_encoder[name].transform(x[[name]]).toarray()
             x.drop(labels=['HomePlanet', 'Destination'], axis='columns', inplace=True)
 
         if self.scaling:
             for name in self.__scaling_encoder.keys():
+                print('----------------------------------')
+                print(x[[name]].describe())
                 enc = self.__scaling_encoder[name]
                 x[name] = enc.transform(x[[name]])
+                print(x[[name]].describe())
 
         x.drop(labels=['PassengerId', 'Cabin', 'CabinNum', 'Name'], axis='columns', inplace=True)
 
         return x
 
 
-'''
-
 data = read_csv("../input/spaceship-titanic-train.csv")
 data = data.sample(frac=1).reset_index(drop=True)
 
-X = DataFrame(data[0:1000])
-Xt = DataFrame(data[1000:1020])
+X = DataFrame(data)
 
 PREPROCESSING = SpaceshipTitanicPreprocessing(
     fill_missing=True,
@@ -147,11 +165,14 @@ PREPROCESSING = SpaceshipTitanicPreprocessing(
     scaling=True,
     one_hot_encoding=True  # 17 features
 )
+set_option('max_columns', None)
 
 # X = PREPROCESSING.fit(X).transform(X)
-X = PREPROCESSING.fit_transform(X)
-Xt = PREPROCESSING.transform(Xt)
 
-print(Xt.isna().sum())
+X = PREPROCESSING.fit(X).transform(X)
 
-'''
+print(X.describe(include='all'))
+
+# Xt = PREPROCESSING.transform(Xt)
+
+# print(Xt.isna().sum())
